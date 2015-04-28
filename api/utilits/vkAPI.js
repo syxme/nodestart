a8e98ce8d506dc20cb76c4af6bf5bdfbb508c248
Dmitry Shebaldin <dima.shebaldin@progforce.com>
2015-04-28 18:01:25 +0300

    ещё кусочек

---
+++ api/utilits/vkAPI.js
@@ -0,0 +1,21 @@
+var http = require('http');
+exports.getcountry = {
+	method	:"get",
+	name	:"getcountry",
+	execute	:function(req, res) {
+		var data = '';
+		http.get("http://api.vk.com/method/database.getCountries?v=5.5&need_all=1&count=1000", function(rews) {
+		 
+
+		  rews.on('data', function (chunk) {
+		  	data+=chunk;
+		  });
+		  rews.on('end', function (chunk) {
+		  	   res.send(data);
+		  });
+ 		 
+ 	   // your code here if you want to use the results !
+ 	  	});
+	}
+}
+	
